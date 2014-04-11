c9ec78952bbf2fee340b7c3df212b6be43ddac56
i386x <sanczes@gmail.com>
2014-04-11 19:07:44 +0200

    added tests for Input, StrInput and FileInput

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -36,7 +36,8 @@ IN THE SOFTWARE.\
 import os
 import unittest
 
-from doit.utils import sys2path, path2sys
+from doit.utils import sys2path, path2sys,\
+                       Input, StrInput, FileInput
 
 class TestPathConversionsCase(unittest.TestCase):
 
@@ -88,8 +89,154 @@ class TestPathConversionsCase(unittest.TestCase):
     #-def
 #-class
 
+class TestInputCase(unittest.TestCase):
+
+    def test_members(self):
+        iname = '<abc>'
+        input = Input(iname)
+        self.assertEqual(input.name, iname)
+        self.assertEqual(input.line, 0)
+    #-def
+
+    def test_methods(self):
+        input = Input('<ab>')
+        self.assertRaises(NotImplementedError, input.getchar)
+        self.assertRaises(NotImplementedError, input.getc)
+        input.ungets('xyz')
+        self.assertEqual(input.getc(), 'x')
+        self.assertEqual(input.getc(), 'y')
+        self.assertEqual(input.getc(), 'z')
+        self.assertRaises(NotImplementedError, input.getc)
+    #-def
+#-class
+
+class TestStrInputCase(unittest.TestCase):
+
+    def test_empty_string(self):
+        nm = '<>'
+        sinput = StrInput("", nm)
+        self.assertEqual(sinput.getc(), "")
+        self.assertEqual(sinput.getc(), "")
+        sinput.ungets("gh")
+        self.assertEqual(sinput.getc(), 'g')
+        self.assertEqual(sinput.getc(), 'h')
+        self.assertEqual(sinput.getc(), "")
+        self.assertEqual(sinput.getc(), "")
+        self.assertEqual(sinput.name, nm)
+        self.assertEqual(sinput.line, 0)
+    #-def
+
+    def test_nonempty_string(self):
+        name = '<mystr>'
+        s = 'ab\nc\n'
+        sinput = StrInput(s, name)
+        self.assertEqual(sinput.getc(), 'a')
+        self.assertEqual(sinput.getc(), 'b')
+        self.assertEqual(sinput.getc(), '\n')
+        self.assertEqual(sinput.getc(), 'c')
+        sinput.ungets('xy')
+        self.assertEqual(sinput.getc(), 'x')
+        self.assertEqual(sinput.getc(), 'y')
+        self.assertEqual(sinput.getc(), '\n')
+        self.assertEqual(sinput.getc(), "")
+        self.assertEqual(sinput.getc(), "")
+        self.assertEqual(sinput.name, name)
+        self.assertEqual(sinput.line, 0)
+    #-def
+#-class
+
+class PseudoFile(object):
+    __slots__ = [ 'name', 'mode', 'closed', '__fpos', '__fsize', '__content' ]
+
+    def __init__(self, content):
+        self.name = None
+        self.mode = None
+        self.closed = True
+        self.__fpos = 0
+        self.__fsize = len(content)
+        self.__content = content
+    #-def
+
+    def open(self, name, mode = 'r'):
+        assert mode in [ 'r', 'w' ], "PseudoFile: Bad mode."
+        assert self.closed, "PseudoFile: Not closed."
+        self.name = name
+        self.mode = mode
+        self.closed = False
+        self.__fpos = 0
+        if mode == 'w':
+            self.__fsize = 0
+            self.__content = type(self.__content)()
+    #-def
+
+    def close(self):
+        assert not self.closed, "PseudoFile: Closed."
+        self.closed = True
+    #-def
+
+    def read(self, size):
+        assert size >= 0, "PseudoFile: Size is negative."
+        assert self.mode == 'r', "PseudoFile: Mode is not for reading."
+        assert not self.closed, "PseudoFile: Closed."
+        assert self.__fpos <= self.__fsize,\
+               "PseudoFile: File pointer value is out of range."
+        s = self.__content[self.__fpos : self.__fpos + size]
+        self.__fpos += len(s)
+        assert self.__fpos <= self.__fsize,\
+               "PseudoFile: File pointer value is out of range."
+        return s
+    #-def
+#-class
+
+class TestFileInputCase(unittest.TestCase):
+
+    def setUp(self):
+        self.__fobjA = PseudoFile("")
+        self.__fobjB = PseudoFile("ab\nc\n")
+        self.__fobjA.open('<empty>')
+        self.__fobjB.open('<test>')
+    #-def
+
+    def test_empty_file(self):
+        finput = FileInput(self.__fobjA)
+        self.assertEqual(finput.getc(), "")
+        self.assertEqual(finput.getc(), "")
+        finput.ungets("gh")
+        self.assertEqual(finput.getc(), 'g')
+        self.assertEqual(finput.getc(), 'h')
+        self.assertEqual(finput.getc(), "")
+        self.assertEqual(finput.getc(), "")
+        self.assertEqual(finput.name, self.__fobjA.name)
+        self.assertEqual(finput.line, 0)
+    #-def
+
+    def test_nonempty_file(self):
+        finput = FileInput(self.__fobjB)
+        self.assertEqual(finput.getc(), 'a')
+        self.assertEqual(finput.getc(), 'b')
+        self.assertEqual(finput.getc(), '\n')
+        self.assertEqual(finput.getc(), 'c')
+        finput.ungets('xy')
+        self.assertEqual(finput.getc(), 'x')
+        self.assertEqual(finput.getc(), 'y')
+        self.assertEqual(finput.getc(), '\n')
+        self.assertEqual(finput.getc(), "")
+        self.assertEqual(finput.getc(), "")
+        self.assertEqual(finput.name, self.__fobjB.name)
+        self.assertEqual(finput.line, 0)
+    #-def
+
+    def tearDown(self):
+        self.__fobjB.close()
+        self.__fobjA.close()
+    #-def
+#-class
+
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestPathConversionsCase))
+    suite.addTest(unittest.makeSuite(TestInputCase))
+    suite.addTest(unittest.makeSuite(TestStrInputCase))
+    suite.addTest(unittest.makeSuite(TestFileInputCase))
     return suite
 #-def
